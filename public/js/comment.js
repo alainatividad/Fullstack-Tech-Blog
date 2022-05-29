f66e9088bcab9ab5281e67f6700129dd9b6d9ffd
AN <99376802+alainatividad@users.noreply.github.com>
2022-05-29 22:53:28 +1000

    Added comments to comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,18 +1,20 @@
 const submitCommentHandler = async (event) => {
   event.preventDefault();
 
+  // get the entry entered into the textbox
   const content = document.querySelector("#content").value.trim();
+  // I have stored the postId into the data-post attribute of comment-card so we'll be getting that to link the comment to the post
   const postId = document.querySelector(".comment").getAttribute("data-post");
-  // console.log("postID! " + postId);
 
   if (content) {
-    // alert("Login!");
+    // crete a post request with content and postId in its body
     const response = await fetch("/api/users/comment", {
       method: "POST",
       body: JSON.stringify({ content, postId }),
       headers: { "Content-Type": "application/json" },
     });
 
+    // if ok, reload the post page
     if (response.ok) {
       document.location.replace(`/post/${postId}`);
     }
