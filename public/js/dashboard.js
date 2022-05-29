322039db02377d1279075fac82a5b10163b5547f
AN <99376802+alainatividad@users.noreply.github.com>
2022-05-29 22:54:18 +1000

    Added comments to dashboard.js and dashboard handlebars

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,14 @@
+// event handlers for the buttons in the dashboard page
+// for create button
 const createPost = async (event) => {
   event.preventDefault();
 
+  // get title and content from the card header and body respectively
   const title = document.querySelector("#title").value.trim();
   const content = document.querySelector("#content").value.trim();
 
   if (content && title) {
+    // create a post request with the title and content in its body
     const response = await fetch("/api/users/post", {
       method: "POST",
       body: JSON.stringify({ title, content }),
@@ -22,9 +26,11 @@ const updatePost = async (event) => {
   const element = event.target;
   const elementType = event.target.getAttribute("type");
 
-  // event.preventDefault();
+  // we're creating this for multiple buttons so let's check if the event really came from a button
   if (element.matches("button")) {
+    // then check if the element type is either a submit or a delete
     if (elementType === "submit") {
+      //if it's a submit, this event is for the Update post
       // get card element of the event target
       const topParent = element.parentElement.parentElement;
       const postId = topParent.getAttribute("data-post");
@@ -35,6 +41,7 @@ const updatePost = async (event) => {
         topParent.firstElementChild.firstElementChild.innerText.trim();
 
       if (content && title) {
+        // put request
         const response = await fetch(`/api/users/post/${postId}`, {
           method: "PUT",
           body: JSON.stringify({ title, content }),
@@ -47,6 +54,7 @@ const updatePost = async (event) => {
         }
       }
     } else if (elementType === "delete") {
+      // if type is delete, then the Delete post is clicked
       // get card element of the event target
       const topParent = element.parentElement.parentElement;
       const postId = topParent.getAttribute("data-post");
@@ -64,7 +72,8 @@ const updatePost = async (event) => {
     }
   }
 };
+
 // check which post triggered the update
 document.querySelector(".posts").addEventListener("click", updatePost);
-
+// event handler for Add post button
 document.querySelector(".new-post").addEventListener("click", createPost);
